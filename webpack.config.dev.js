6be0f4a2e3412a374c28ed65950712895bf34c65
IIpocTo <allxf95@gmail.com>
2017-03-15 17:40:38 +0300

    Unite server and client build

---
+++ webpack.config.dev.js
@@ -0,0 +1,81 @@
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  output: {
+    path: '/'
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client'),
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000'
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: 'file-loader'
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'client/public/index.html'),
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: [
+              '>1%',
+              'last 4 versions',
+              'Firefox ESR',
+              'not ie < 9',
+            ]
+          })
+        ]
+      }
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+}
